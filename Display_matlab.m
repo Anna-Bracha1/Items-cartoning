47539aa2764387a3b75f9ef30801e958a0d9215c
Anna-Bracha1 <33527752+Anna-Bracha1@users.noreply.github.com>
2017-12-17 12:20:28 +0100

    Add files via upload

--- Display_matlab.m
+++ Display_matlab.m
@@ -1,23 +1,38 @@
+clear all;
+close all;
 
-formatSpec = '%d %d %d %d %d %d %d %d %d %d';
+numOfBoxes = 6;
 
 filename = 'Boxes.txt';
-fileID = fopen('Items.txt','r');
+fileID = fopen(filename','r');
 
 formatSpec = '%d %d %d %d %d %d %d %d %d %d';
-sizeA = [10 10];
+sizeBox = [10 10];
+
+Box = zeros(10, 10, numOfBoxes);
 
 for i = 1:numOfBoxes
-	Box(i) = fscanf(fileID, formatSpec, sizeA);
-	Box(i) = Box(i)';
+	Box(:,:,i) = fscanf(fileID, formatSpec, sizeBox);
+	Box(:,:,i) = Box(:,:,i)';
 end
 
 fclose(fileID);
 
-figure;
-for i = 1:numOfBoxes
-	subplot(4, 4, i);
-	image(Box(i), 'CDataMapping', 'scaled');
-end
+mymap = colormap(hsv);  
+mymap(1,:) = [0 0 0];   %make first color black
 
-%B = imresize(A, scale);
+figure(1);
+colormap(mymap);
+for i = 1:numOfBoxes
+    subplot(2, 4, i);
+    image(Box(:,:,i), 'CDataMapping', 'scaled');
+    title(['Box ' num2str(i)]);
+    
+    stats = regionprops(Box(:,:,i), 'centroid', 'BoundingBox');
+    for j = 1:length(stats)
+        if(isnan(stats(j).Centroid(1)) == 0)
+            text(stats(j).Centroid(1), stats(j).Centroid(2),['\color{black}', num2str(j)]);
+            rectangle('Position', stats(j).BoundingBox);
+        end
+    end
+end
